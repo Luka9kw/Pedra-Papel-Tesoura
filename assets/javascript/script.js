566b392834e483e30aaacfe8d53ec1a16690e2f6
luka9kw <luka9kw@hotmail.com>
2023-05-09 16:05:36 -0300

    logica ppt finalizada

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,7 @@
 const inputPapel = document.querySelector('#papel');
 const inputPedra = document.querySelector('#pedra');
 const inputTesoura = document.querySelector('#tesoura');
+const res = document.querySelector('#resultado');
 
 let pedra = '';
 let papel = '';
@@ -10,6 +11,7 @@ inputPedra.addEventListener('click', function () {
     pedra = true;
     papel = false;
     tesoura = false;
+    resultado();
 })
 
 
@@ -17,24 +19,49 @@ inputPapel.addEventListener('click', function () {
     pedra = false;
     papel = true;
     tesoura = false;
+    resultado();
 })
 
 inputTesoura.addEventListener('click', function () {
     pedra = false;
     papel = false;
     tesoura = true;
+    resultado();
 })
 
-function escolhaCPU() {
+function resultado() {
     let PPT = ['pedra', 'papel', 'tesoura'];
     let cpuPPT = Math.floor(Math.random() * PPT.length);
     let randomCPU = PPT[cpuPPT];
 
-}
-
-function resultado() {
-    if(pedra && randomCPU) {
-        console.log('você venceu');
+    // Condições de Vitória
+    if (randomCPU == 'pedra' && papel) {
+        final.innerHTML = 'Você venceu!'
+    }
+    else if (randomCPU == 'papel' && tesoura) {
+        final.innerHTML = 'Você venceu!'
+    }
+    else if (randomCPU == 'tesoura' && pedra) {
+        final.innerHTML = 'Você venceu!'
     }
 
+    // Condições de Empate
+    else if (randomCPU == 'pedra' && pedra) {
+        final.innerHTML = 'Empate!'
+    }
+    else if (randomCPU == 'papel' && papel) {
+        final.innerHTML = 'Empate!'
+    }
+    else if (randomCPU == 'tesoura' && tesoura) {
+        final.innerHTML = 'Empate!'
+    }
+
+    // Condições de Derrota
+    else {
+        final.innerHTML = 'Você perdeu!'
+    }
 }
+
+const final = document.createElement('p');
+res.appendChild(final);
+
